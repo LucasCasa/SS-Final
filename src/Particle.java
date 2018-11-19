c69f41a648940ca42839a1e1772b8ae6571c37ef
Nico Castano <nicolasm.castano@gmail.com>
2018-11-19 18:28:19 -0300

    Driving, Granular and Social Forces. Particle or Vector2 needs velocity vector. Granular Force needs some changes.

--- src/Particle.java
+++ src/Particle.java
@@ -1,6 +1,6 @@
+import jdk.internal.util.xml.impl.Pair;
 import vector.Vector2;
 
-import java.awt.Point;
 import java.util.List;
 
 public class Particle {
@@ -53,6 +53,65 @@ public class Particle {
 			dir.add(position.x - neighbour.position.x, position.y - neighbour.position.y);
 		}
 		return dir.nor();
+	}
+
+	public Vector2[] getGranularForce(Particle p) {
+		Vector2 direction = new Vector2(position.x - p.position.x, position.y - p.position.y);
+		double epsilon = direction.len() - p.getCurrentRadius() + getCurrentRadius();
+		if(epsilon < 0){
+			return new Vector2[]{new Vector2(0,0),new Vector2(0,0)};
+		}
+		direction.nor();
+
+		//Componente normal de la Fuerza
+		direction.scl((float) (-epsilon*(1.2*100000))); //Kn = 1.2*10^5 N/m
+
+		//Componente tangencial de la Fuerza
+		//Vector2 relativeVelocity = vectorVelocidad - p.velocity
+		//Vector2 tangencial = new Vector2(-direction.y, direction.x)
+		//double velTangencial = relativeVelocity.x*tangencial.x + relativeVelocity.y*tangencial.y;
+		//tangencial.scl((float)(-(2.4*100000)*epsilon*velTangencial)) // Kt = 2.4*10^5 kg/(m/s)
+		Vector2 tang = new Vector2(0,0);
+
+		return new Vector2[]{direction, tang}; //direction seria la direccion normal. tang habria que modificarlo solucionando lo que esta comentado.
+	}
+
+	public Vector2 getSocialForce(Particle p) {
+		Vector2 direction = new Vector2(position.x - p.position.x, position.y - p.position.y);
+		double epsilon = direction.len() - p.getCurrentRadius() - getCurrentRadius();
+
+		if(epsilon > 1) {
+			return new Vector2(0,0);
+		}
+
+		direction.nor();
+		Vector2 socialForce = direction.cpy();
+		socialForce.scl((float) (2000*Math.exp(-epsilon/0.08))); //A = 2000 N; B = 0.08 m
+
+		return socialForce;
+	}
+
+	public Vector2 getDrivingForce() {
+
+		//HABRIA QUE HACER ALGUN CHEQUEO ANTES CON RESPECTO A LA POSICION ?
+
+		Vector2 direction = new Vector2(position.x - target.x, position.y - target.y);
+		direction.nor();
+		Vector2 aux = direction.cpy();
+		aux.scl((float) 0.8); //Velocidad deseada varia entre 0.8 y 6 m/s;
+
+		Vector2 drivingForce =  new Vector2(aux.x - speed, aux.y - speed); //en vez de speed tiene que ir el vector velocidad
+		drivingForce.scl(mass/TAU);
+
+		return drivingForce;
+	}
+
+	public Vector2[] getForce(Particle p) {
+		Vector2[] granularForce = getGranularForce(p);
+		Vector2 socialForce = getSocialForce(p);
+		Vector2 normal = new Vector2(granularForce[0].x + socialForce.x, granularForce[0].y + socialForce.y);
+
+		return new Vector2[]{normal, granularForce[1]};
 	}
 
 	public String toString() {
